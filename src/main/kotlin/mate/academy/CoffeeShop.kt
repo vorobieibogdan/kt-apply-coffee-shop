b02fac45a88d7950dde342815900e0528ed41782
Bohdan Vorobiei <vbs@ukr.net>
2026-02-20 03:00:01 +0200

    task completed

--- src/main/kotlin/mate/academy/CoffeeShop.kt
+++ src/main/kotlin/mate/academy/CoffeeShop.kt
@@ -5,7 +5,11 @@ class CoffeeShop {
 
     // Method to sell coffee to a client
     fun sellCoffee(clientName: String, coffeeType: String): CoffeeCup {
-        // Create and initialize a CoffeeCup, then apply the serve action
-
+        return CoffeeCup().apply {
+            this.clientName = clientName
+            this.coffeeType = coffeeType
+            this.isServed = true
+            serveCoffee()
+        }
     }
 }
